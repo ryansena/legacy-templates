033f631a446dfa164e08243c25a03cabdcd4e421
ryansena <46745367+ryansena@users.noreply.github.com>
2019-08-22 17:00:36 +0800

    Update index.js

--- sp/index.js
+++ sp/index.js
@@ -1,11 +1,7 @@
 import dynamic from "template-utils/dynamic";
 import addDirToTemplatePath from "template-utils/addDirToTemplatePath";
-/* import dynamic from "next/dynamic"
-import addDirToTemplatePath from "../../../addDirToTemplatePath"
-
-const SP2019MAIN = dynamic( */
 const SP2019MAIN = dynamic(() =>
-  import("./SP-2019-MAIN" /* webpackChunkName: "SPTemplates" */)
+  import("./SP-2019-MAIN")
 );
 
 const templates = {
